1988de0b10a96e382fb3601dfcfea7973822577b
Michael <michaelsmike7@yahoo.com>
2024-02-05 10:02:53 -0500

    added user input unfinished

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# create function for user choice
+# display everything in cart that they have picked so far
+# display the line total(item price * quantity
+# Ask if they want to redisplay menu and complete the purchase
+
+
+def user_choice(choice):
+    return ""
+
+
+def cart_items(cart, quantity):
+    for x, y in zip(cart, quantity):
+        print(f'you ordered {y} {x}(s)')
+        return ""
