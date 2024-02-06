00f49d078ee8f160163e593e36c3bea0418061e7
Michael <michaelsmike7@yahoo.com>
2024-02-06 11:39:46 -0500

    fixed shopping cart

--- user_input.py
+++ user_input.py
@@ -1,7 +1,8 @@
+# create function for user choice
 # display everything in cart that they have picked so far
 
 
 def cart_items(cart, quantity):
     for x, y in zip(cart, quantity):
         print(f'{y} {x}(s) in your cart')
-        return ""
+    return ""
